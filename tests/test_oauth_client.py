77f5a14c62a754769ce7eef7d31bb012721884e2
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-01-03 10:40:29 -0500

    refactor unittest

--- tests/test_oauth_client.py
+++ tests/test_oauth_client.py
@@ -22,11 +22,12 @@ from quart_authlib import QuartOAuth2App
 
 
 class QuartOAuthTest(IsolatedAsyncioTestCase):
-    async def test_register_remote_app(self):
-        app = Quart(__name__)
-        oauth = OAuth(app)
-        self.assertRaises(AttributeError, lambda: oauth.dev)
+    def setUp(self):
+        self.app = Quart(__name__)
 
+    async def test_register_remote_app(self):
+        oauth = OAuth(self.app)
+        self.assertRaises(AttributeError, lambda: oauth.dev)
         oauth.register(
             "dev",
             client_id="dev",
@@ -36,27 +37,25 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
         self.assertEqual(oauth.dev.client_id, "dev")
 
     async def test_register_conf_from_app(self):
-        app = Quart(__name__)
-        app.config.update(
+        self.app.config.update(
             {
                 "DEV_CLIENT_ID": "dev",
                 "DEV_CLIENT_SECRET": "dev",
             }
         )
-        oauth = OAuth(app)
+        oauth = OAuth(self.app)
         oauth.register("dev")
         self.assertEqual(oauth.dev.client_id, "dev")
 
     async def test_register_with_overwrite(self):
-        app = Quart(__name__)
-        app.config.update(
+        self.app.config.update(
             {
                 "DEV_CLIENT_ID": "dev-1",
                 "DEV_CLIENT_SECRET": "dev",
                 "DEV_ACCESS_TOKEN_PARAMS": {"foo": "foo-1"},
             }
         )
-        oauth = OAuth(app)
+        oauth = OAuth(self.app)
         oauth.register(
             "dev", overwrite=True, client_id="dev", access_token_params={"foo": "foo"}
         )
@@ -65,8 +64,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
         self.assertEqual(oauth.dev.access_token_params["foo"], "foo-1")
 
     async def test_init_app_later(self):
-        app = Quart(__name__)
-        app.config.update(
+        self.app.config.update(
             {
                 "DEV_CLIENT_ID": "dev",
                 "DEV_CLIENT_SECRET": "dev",
@@ -75,7 +73,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
         oauth = OAuth()
         remote = oauth.register("dev")
         self.assertRaises(RuntimeError, lambda: oauth.dev.client_id)
-        oauth.init_app(app)
+        oauth.init_app(self.app)
         self.assertEqual(oauth.dev.client_id, "dev")
         self.assertEqual(remote.client_id, "dev")
 
@@ -84,9 +82,8 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
         self.assertIsNone(oauth.update_token)
 
     async def test_init_app_params(self):
-        app = Quart(__name__)
         oauth = OAuth()
-        oauth.init_app(app, SimpleCache())
+        oauth.init_app(self.app, SimpleCache())
         self.assertIsNotNone(oauth.cache)
         self.assertIsNone(oauth.update_token)
 
@@ -94,15 +91,13 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
         self.assertIsNotNone(oauth.update_token)
 
     async def test_create_client(self):
-        app = Quart(__name__)
-        oauth = OAuth(app)
+        oauth = OAuth(self.app)
         self.assertIsNone(oauth.create_client("dev"))
         oauth.register("dev", client_id="dev")
         self.assertIsNotNone(oauth.create_client("dev"))
 
     async def test_register_oauth1_remote_app(self):
-        app = Quart(__name__)
-        oauth = OAuth(app)
+        oauth = OAuth(self.app)
         client_kwargs = dict(
             client_id="dev",
             client_secret="dev",
@@ -117,16 +112,15 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
         self.assertEqual(oauth.dev.name, "dev")
         self.assertEqual(oauth.dev.client_id, "dev")
 
-        oauth = OAuth(app, cache=SimpleCache())
+        oauth = OAuth(self.app, cache=SimpleCache())
         oauth.register("dev", **client_kwargs)
         self.assertEqual(oauth.dev.name, "dev")
         self.assertEqual(oauth.dev.client_id, "dev")
 
     async def test_oauth1_authorize_cache(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
+        self.app.secret_key = "!"
         cache = SimpleCache()
-        oauth = OAuth(app, cache=cache)
+        oauth = OAuth(self.app, cache=cache)
 
         client = oauth.register(
             "dev",
@@ -138,7 +132,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             authorize_url="https://i.b/authorize",
         )
 
-        async with app.test_request_context("/"):
+        async with self.app.test_request_context("/"):
             with mock.patch("requests.sessions.Session.send") as send:
                 send.return_value = mock_send_value(
                     "oauth_token=foo&oauth_verifier=baz"
@@ -148,7 +142,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
                 url = resp.headers.get("Location")
                 self.assertIn("oauth_token=foo", url)
 
-        async with app.test_request_context("/?oauth_token=foo"):
+        async with self.app.test_request_context("/?oauth_token=foo"):
             with mock.patch("requests.sessions.Session.send") as send:
                 send.return_value = mock_send_value(
                     "oauth_token=a&oauth_token_secret=b"
@@ -157,9 +151,8 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
                 self.assertEqual(token["oauth_token"], "a")
 
     async def test_oauth1_authorize_session(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
-        oauth = OAuth(app)
+        self.app.secret_key = "!"
+        oauth = OAuth(self.app)
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -170,7 +163,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             authorize_url="https://i.b/authorize",
         )
 
-        async with app.test_request_context("/"):
+        async with self.app.test_request_context("/"):
             with mock.patch("requests.sessions.Session.send") as send:
                 send.return_value = mock_send_value(
                     "oauth_token=foo&oauth_verifier=baz"
@@ -181,7 +174,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
                 self.assertIn("oauth_token=foo", url)
                 data = session["_state_dev_foo"]
 
-        async with app.test_request_context("/?oauth_token=foo"):
+        async with self.app.test_request_context("/?oauth_token=foo"):
             session["_state_dev_foo"] = data
             with mock.patch("requests.sessions.Session.send") as send:
                 send.return_value = mock_send_value(
@@ -191,8 +184,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
                 self.assertEqual(token["oauth_token"], "a")
 
     async def test_register_oauth2_remote_app(self):
-        app = Quart(__name__)
-        oauth = OAuth(app)
+        oauth = OAuth(self.app)
         oauth.register(
             "dev",
             client_id="dev",
@@ -208,9 +200,8 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
         self.assertIsNotNone(session.update_token)
 
     async def test_oauth2_authorize(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
-        oauth = OAuth(app)
+        self.app.secret_key = "!"
+        oauth = OAuth(self.app)
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -220,7 +211,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             authorize_url="https://i.b/authorize",
         )
 
-        async with app.test_request_context("/"):
+        async with self.app.test_request_context("/"):
             resp = client.authorize_redirect("https://b.com/bar")
             self.assertEqual(resp.status_code, 302)
             url = resp.headers.get("Location")
@@ -229,10 +220,9 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             self.assertIsNotNone(state)
             data = session[f"_state_dev_{state}"]
 
-        async with app.test_request_context(path=f"/?code=a&state={state}"):
+        async with self.app.test_request_context(path=f"/?code=a&state={state}"):
             # session is cleared in tests
             session[f"_state_dev_{state}"] = data
-
             with mock.patch("requests.sessions.Session.send") as send:
                 send.return_value = mock_send_value(get_bearer_token())
                 token = await client.authorize_access_token()
@@ -242,9 +232,8 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             self.assertEqual(client.token, None)
 
     async def test_oauth2_authorize_access_denied(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
-        oauth = OAuth(app)
+        self.app.secret_key = "!"
+        oauth = OAuth(self.app)
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -254,7 +243,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             authorize_url="https://i.b/authorize",
         )
 
-        async with app.test_request_context(
+        async with self.app.test_request_context(
             path="/?error=access_denied&error_description=Not+Allowed"
         ):
             # session is cleared in tests
@@ -265,10 +254,8 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
     async def test_oauth2_authorize_via_custom_client(self):
         class CustomRemoteApp(QuartOAuth2App):
             OAUTH_APP_CONFIG = {"authorize_url": "https://i.b/custom"}
-
-        app = Quart(__name__)
-        app.secret_key = "!"
-        oauth = OAuth(app)
+        self.app.secret_key = "!"
+        oauth = OAuth(self.app)
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -277,16 +264,15 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             access_token_url="https://i.b/token",
             client_cls=CustomRemoteApp,
         )
-        async with app.test_request_context("/"):
+        async with self.app.test_request_context("/"):
             resp = client.authorize_redirect("https://b.com/bar")
             self.assertEqual(resp.status_code, 302)
             url = resp.headers.get("Location")
             self.assertTrue(url.startswith("https://i.b/custom?"))
 
     async def test_oauth2_authorize_with_metadata(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
-        oauth = OAuth(app)
+        self.app.secret_key = "!"
+        oauth = OAuth(self.app)
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -309,14 +295,13 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
                 {"authorization_endpoint": "https://i.b/authorize"}
             )
 
-            async with app.test_request_context("/"):
+            async with self.app.test_request_context("/"):
                 resp = client.authorize_redirect("https://b.com/bar")
                 self.assertEqual(resp.status_code, 302)
 
     async def test_oauth2_authorize_code_challenge(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
-        oauth = OAuth(app)
+        self.app.secret_key = "!"
+        oauth = OAuth(self.app)
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -326,7 +311,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             client_kwargs={"code_challenge_method": "S256"},
         )
 
-        async with app.test_request_context("/"):
+        async with self.app.test_request_context("/"):
             resp = client.authorize_redirect("https://b.com/bar")
             self.assertEqual(resp.status_code, 302)
             url = resp.headers.get("Location")
@@ -345,7 +330,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             return mock_send_value(get_bearer_token())
 
         path = f"/?code=a&state={state}"
-        async with app.test_request_context(path=path):
+        async with self.app.test_request_context(path=path):
             # session is cleared in tests
             session[f"_state_dev_{state}"] = data
 
@@ -354,12 +339,10 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
                 self.assertEqual(token["access_token"], "a")
 
     async def test_openid_authorize(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
-        oauth = OAuth(app)
+        self.app.secret_key = "!"
+        oauth = OAuth(self.app)
         params = dict(kty="oct", kid="f")
         key = dict(JsonWebKey.import_key("secret", params))
-
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -370,7 +353,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             jwks={"keys": [key]},
         )
 
-        async with app.test_request_context("/"):
+        async with self.app.test_request_context("/"):
             resp = client.authorize_redirect("https://b.com/bar")
             self.assertEqual(resp.status_code, 302)
 
@@ -405,9 +388,8 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
                 self.assertIn("userinfo", token)
 
         async def test_oauth2_access_token_with_post(self):
-            app = Quart(__name__)
-            app.secret_key = "!"
-            oauth = OAuth(app)
+            self.app.secret_key = "!"
+            oauth = OAuth(self.app)
             client = oauth.register(
                 "dev",
                 client_id="dev",
@@ -417,7 +399,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
                 authorize_url="https://i.b/authorize",
             )
             payload = {"code": "a", "state": "b"}
-            async with app.test_request_context(path="/", data=payload, method="POST"):
+            async with self.app.test_request_context(path="/", data=payload, method="POST"):
                 session["_state_dev_b"] = {"data": payload}
                 with mock.patch("requests.sessions.Session.send") as send:
                     send.return_value = mock_send_value(get_bearer_token())
@@ -425,12 +407,10 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
                     self.assertEqual(token["access_token"], "a")
 
     async def test_access_token_with_fetch_token(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
+        self.app.secret_key = "!"
         oauth = OAuth()
-
         token = get_bearer_token()
-        oauth.init_app(app, fetch_token=lambda name: token)
+        oauth.init_app(self.app, fetch_token=lambda name: token)
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -448,20 +428,17 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             resp.status_code = 200
             return resp
 
-        async with app.test_request_context("/"):
+        async with self.app.test_request_context("/"):
             with mock.patch("requests.sessions.Session.send", fake_send):
                 resp = client.get("/api/user")
                 self.assertEqual(resp.text, "hi")
-
                 # trigger ctx.authlib_client_oauth_token
                 resp = client.get("/api/user")
                 self.assertEqual(resp.text, "hi")
 
     async def test_request_with_refresh_token(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
+        self.app.secret_key = "!"
         oauth = OAuth()
-
         expired_token = {
             "token_type": "Bearer",
             "access_token": "expired-a",
@@ -469,7 +446,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             "expires_in": "3600",
             "expires_at": 1566465749,
         }
-        oauth.init_app(app, fetch_token=lambda name: expired_token)
+        oauth.init_app(self.app, fetch_token=lambda name: expired_token)
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -494,15 +471,14 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             resp.status_code = 200
             return resp
 
-        async with app.test_request_context("/"):
+        async with self.app.test_request_context("/"):
             with mock.patch("requests.sessions.Session.send", fake_send):
                 resp = client.get("/api/user", token=expired_token)
                 self.assertEqual(resp.text, "hi")
 
     async def test_request_without_token(self):
-        app = Quart(__name__)
-        app.secret_key = "!"
-        oauth = OAuth(app)
+        self.app.secret_key = "!"
+        oauth = OAuth(self.app)
         client = oauth.register(
             "dev",
             client_id="dev",
@@ -520,7 +496,7 @@ class QuartOAuthTest(IsolatedAsyncioTestCase):
             resp.status_code = 200
             return resp
 
-        async with app.test_request_context("/"):
+        async with self.app.test_request_context("/"):
             with mock.patch("requests.sessions.Session.send", fake_send):
                 resp = client.get("/api/user", withhold_token=True)
                 self.assertEqual(resp.text, "hi")
